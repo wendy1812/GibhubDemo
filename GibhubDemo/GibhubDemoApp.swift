adf63db57e8e3f1884a2f928833dae646d324d06
wendy1812 <trumlatakaka1@gmail.com>
2023-12-07 11:15:58 +0700

    Initial Commit

---
+++ GibhubDemo/GibhubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GibhubDemoApp.swift
+//  GibhubDemo
+//
+//  Created by mac on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GibhubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
